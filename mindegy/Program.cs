1079af93f731f62828037b94e3f3d9a0a99e44cb
szabo.patrik@vereb.dc <szabo.patrik@vereb.dc>
2019-09-20 12:00:54 +0200

    Add project files.

---
+++ mindegy/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mindegy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Szia, hogy hívnak?");
+
+            var nev = Console.ReadLine();
+            Console.WriteLine($"Oh milyen szép név az, hogy {nev}");
+            Console.WriteLine(" Szeretsz programozni?");
+            var valasz = Console.ReadLine();
+            if (valasz.ToLower() == "igen")
+            {
+                Console.WriteLine("Akkor mi nagyon jó barátok leszünk!");
+            }
+            else
+            {
+                Console.WriteLine(" :(");
+                Console.WriteLine("Akkor viszlát");
+            }
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
